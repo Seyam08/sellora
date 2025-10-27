e7c6596068febf358c088a89896c10cf4827a390
Seyam <seyam.hossain.118833@gmail.com>
2025-10-27 12:39:39 +0600

    feat (graphql) - added codegen for generating types from graphql schema. Also integrated watch mode

--- src/graphQL/resolvers/user.ts
+++ src/graphQL/resolvers/user.ts
@@ -1,17 +1,29 @@
-export const userResolvers = {
+import {
+  Greetings,
+  MutationCreateUserArgs,
+  MutationUpdateUserArgs,
+  Resolvers,
+  Test,
+  User,
+} from "@/graphQL/graphQL-Types";
+
+export const userResolvers: Resolvers = {
   Query: {
-    greetings: () => ({ first: "Hello", last: "World" }),
+    greetings: (): Greetings => ({ first: "Hello", last: true }),
+    hello: (): Test => {
+      return { text: "he is a liar!", state: true };
+    },
   },
   Mutation: {
-    createUser: (_: unknown, args: { name: string }) => {
+    createUser: (_: unknown, args: MutationCreateUserArgs) => {
       return {
         id: "dsg",
         name: args.name,
       };
     },
-    updateUser: (_: unknown, args: { name?: string }) => {
+    updateUser: (_: unknown, args: MutationUpdateUserArgs): User => {
       return {
-        id: 124124,
+        id: "124124",
         name: args?.name ?? "",
       };
     },
--- src/graphQL/typeDefs/index.ts
+++ src/graphQL/typeDefs/index.ts
@@ -1,5 +1,11 @@
-import { userTypeDefs } from "@/graphQL/typeDefs/user";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLSchema } from "graphql";
 
-export const typeDefs = /* GraphQL */ `
-  ${userTypeDefs}
-`;
+export const typeDefs: GraphQLSchema = loadSchemaSync("./**/*.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
+
+// export const typeDefs = /* GraphQL */ `
+//   ${userTypeDefs}
+// `;
