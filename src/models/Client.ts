3928aff7168c537eb272270fe7fc3deb6ecff81a
Seyam <seyam.hossain.118833@gmail.com>
2025-10-31 09:16:14 +0600

    feat(DB schema): add Client model with schema definition for MongoDB

---
+++ src/models/Client.ts
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+
+export const ClientSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: [true, "Unique id is required!"],
+      unique: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: [true, "Please provide a Username!"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    name: String,
+    email: {
+      type: String,
+      required: [true, "Please provide a unique email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+      trim: true,
+      unique: true,
+    },
+    address: String,
+    orderHistory: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Order",
+        },
+      ],
+      default: [],
+    },
+    avatarUrl: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Client =
+  mongoose.models.Client ?? mongoose.model("Client", ClientSchema);
+
+// type ClientType = InferSchemaType<typeof ClientSchema>;
