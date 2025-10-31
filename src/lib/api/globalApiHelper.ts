9d9342ac89af4a0b7b6796289ce201ca3e7bd411
Seyam <seyam.hossain.118833@gmail.com>
2025-10-31 17:06:27 +0600

    feat(utility) : added helper function for extraction zod form error

--- src/lib/api/globalApiHelper.ts
+++ src/lib/api/globalApiHelper.ts
@@ -9,3 +9,15 @@ export const checkContentType = (contentType: string | null): ContentReturn => {
   }
   return "invalid";
 };
+
+export const getZodFormError = (formErrors: Array<string>): Array<string> => {
+  let unknownKeys: string[] = [];
+
+  if (formErrors.length > 0) {
+    const match = formErrors[0].match(/"([^"]+)"/g);
+    if (match) {
+      unknownKeys = match.map((key) => key.replace(/"/g, ""));
+    }
+  }
+  return unknownKeys;
+};
